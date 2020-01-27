bce119a8dfe44bb36b8736275f9acdc569b10198
lakshmipriyav <30892252+lakshmipriyav@users.noreply.github.com>
2020-01-27 16:49:17 +0530

    Create PercentageChange.java
    Percentage Change of stock price

---
+++ PercentageChange.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.Math; 
+class Percentage{
+    String sname,ssym;
+    double pprice,cprice;
+    Percentage(String sname,String ssym,double pprice,double cprice)
+    {
+        this.sname=sname;
+        this.ssym=ssym;
+        this.pprice=pprice;
+        this.cprice=cprice;
+    }
+   double getChangePercentage()
+    {
+        
+        return ((pprice-cprice)/100);
+    }
+}
+
+public class PercentageChange {
+    public static void main(String args[]) {
+   Scanner sc=new Scanner(System.in);
+   String sname,ssym;
+    double pprice,cprice;
+   sname=sc.next();
+   ssym=sc.next();
+   pprice=sc.nextDouble();
+   cprice=sc.nextDouble();
+Percentage p=new Percentage(sname,ssym,pprice,cprice);
+double per=p.getChangePercentage();
+if(per<0)
+System.out.println(Math.abs(per)+" percentage decreased ");
+else
+System.out.println(Math.abs(per)+" percentage increased ");
+
+        
+    }
+}

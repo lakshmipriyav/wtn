2703086d0651aab04554cd75b2ec716df4a7db3b
lakshmipriyav <30892252+lakshmipriyav@users.noreply.github.com>
2020-01-27 15:53:20 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,22 @@
+public class MyClass {
+    public static void main(String args[]) {
+   
+if(args.length<9)
+{
+   System.out.println("Please enter 9 integer numbers");
+}
+else
+
+{
+    int max=Integer.parseInt(args[0]);
+    for(int i=1;i<args.length;i++)  
+
+if(max<Integer.parseInt(args[i]))
+{
+    max=Integer.parseInt(args[i]);
+}
+  System.out.println("The biggest number in the given array is "+max); 
+}  
+    
+    }
+}

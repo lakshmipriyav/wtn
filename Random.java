80458aaca270da3226866f37a113740883e1dc69
lakshmipriyav <30892252+lakshmipriyav@users.noreply.github.com>
2020-01-29 08:01:35 +0530

    Update Random.java

--- Random.java
+++ Random.java
@@ -1,16 +1,40 @@
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
- 
- class RandomHelper{
-    static int RandInt() {Scanner sc= new Scanner(System.in);      int x=sc.nextInt();
-       int y=sc.nextInt();
-	int randomInt = ThreadLocalRandom.current().nextInt(x, y+1);
-	return randomInt;
-	 }}
-public class Random {
- 
-   public static void main(String[] args) {
-       RandomHelper r=new RandomHelper();
-	System.out.println("Random number generated is : " +r.RandInt());
-   }}
-   
+class RandomHelper 
+{
+static int rand;
+static Double rand1;
+static void randint()
+{
+Scanner sc= new Scanner(System.in);
+int p=sc.nextInt();
+int q=sc.nextInt();
+rand = (int)(Math.random() * (q-p)) +p;
+
+}
+static void randdouble(int c,int d)
+{
+rand1 = (Math.random() * (d-c)) +c;
+
+}
+void display()
+{
+System.out.println("random int is:"+rand);
+}
+void display1()
+{
+System.out.println("random double is:"+rand1);
+}
+}
+
+public class MyClass{
+public static void main(String [] args)
+{
+RandomHelper r=new RandomHelper();
+RandomHelper.randint();
+RandomHelper.randdouble(7,10);
+r.display();
+r.display1();
+
+} 
+
+}
